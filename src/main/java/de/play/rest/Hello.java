2edbe86e4c96350cca4b52acd4dc6b3e5216c6c8
Michael Felix <milix@mail.de>
2024-12-01 15:41:09 +0100

    add hello world rest resource

---
+++ src/main/java/de/play/rest/Hello.java
@@ -0,0 +1,14 @@
+package de.play.rest;
+
+public class Hello {
+
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getHello() {
+        return name;
+    }
+}
